65b6dafb58825b73d08c20b996b3ed7971b761e4
nall <jon.nall@gmail.com>
2009-11-10 18:58:44 -0700

    Removed superfluous notification resize/move code. As a child window it does this automatically.

--- SZOverlayWindow.m
+++ SZOverlayWindow.m
@@ -34,10 +34,6 @@
 
 #import "SZOverlayWindow.h"
 
-@interface SZOverlayWindow(Private)
--(void)updateFrame:(NSNotification*)notification;
-@end
-
 @implementation SZOverlayWindow
 
 -(id)initWithParentWindow:(NSWindow*)parentWindow
@@ -71,18 +67,6 @@
         // Add ourself to the parent
         [parentWindow addChildWindow:self
                              ordered:NSWindowAbove];  
-        
-        // Observe our parent window's resize and move notifications
-        // so we can resize / move ourself appropriately
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(updateFrame:)
-                                                     name:NSWindowDidMoveNotification
-                                                   object:parentWindow];
-
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(updateFrame:)
-                                                     name:NSWindowDidResizeNotification
-                                                   object:parentWindow];
     }
     
     return self;
@@ -94,20 +78,4 @@
     return YES;
 }
 
-@end
-
-@implementation SZOverlayWindow(Private)
-
-// Callback for when our parent window moves or resizes.
-// Just adjust ourselves to sit inside its contentView rect.
--(void)updateFrame:(NSNotification*)notification
-{
-    NSWindow* parentWindow = [self parentWindow];
-    NSRect overlayRect = NSMakeRect(parentWindow.frame.origin.x,
-                                    parentWindow.frame.origin.y, 
-                                    [[parentWindow contentView] frame].size.width,
-                                    [[parentWindow contentView] frame].size.height);
-    [self setFrame:overlayRect
-           display:YES];
-}
 @end
