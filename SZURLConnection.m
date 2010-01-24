a41309e8f55a4393c302df8f253a69bdbd6fc55b
Jon Nall <jon.nall@gmail.com>
2010-01-24 12:07:18 -0800

    Fixed various copyright notices and headers

--- SZURLConnection.m
+++ SZURLConnection.m
@@ -2,7 +2,7 @@
 //  SZURLConnection.h
 //
 //  Created by Jon Nall on 10/9/09.
-//  Copyright 2009 STUNTAZ!!!. All rights reserved.
+//  Copyright 2009-2010 STUNTAZ!!!. All rights reserved.
 // 
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are met:
